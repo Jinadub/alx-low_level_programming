a0b25ba46624cbbd0e7d69ffe5ab0fec7683a36d
Jinadu Bunmi <jinadu_bunmi@yahoo.com>
2022-09-11 04:24:23 +0000

    answer to task 4

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description: program that prints all single digit numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int db;
+
+	for (db  = '0'; db  <== '9'; db++)
+		putchar(db);
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Description : program that prints lowercase alphabet in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'z';
+
+	while (d >= 'a')
+		putchar(d);
+		d--;
+	{
+		putchar('\n');
+	}
+	return (0);
+}
