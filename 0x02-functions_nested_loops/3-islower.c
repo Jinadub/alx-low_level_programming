05720912487c58fc6205e716686a4d702ac1b80a
Jinadu Bunmi <jinadu_bunmi@yahoo.com>
2022-09-13 23:46:24 +0000

    lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: Characters in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (1);
+	}
+	return (0);
+}
