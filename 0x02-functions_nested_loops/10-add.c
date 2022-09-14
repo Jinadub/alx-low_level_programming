4c60cb61e9dfc29c537c283cfa39e44712694449
Jinadu Bunmi <jinadu_bunmi@yahoo.com>
2022-09-14 03:01:16 +0000

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: Integar to be added
+ * @b: Integar to be added
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = (a + b);
+
+	return (sum);
+}
