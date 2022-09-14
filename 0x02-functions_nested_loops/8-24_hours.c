18ef89612ccd883e07959a4af71762fb29518d9d
Jinadu Bunmi <jinadu_bunmi@yahoo.com>
2022-09-14 02:29:16 +0000

    minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of jack bauer
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int hr = 0, min;
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar('0' + (hr / 10));
+			_putchar('0' + (hr % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
+		}
+		_putchar('\n');
+		hr++;
+	}
+}
