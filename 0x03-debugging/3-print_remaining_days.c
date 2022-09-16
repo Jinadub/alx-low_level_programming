52b4a0059a72f9016d9d624d1f0b3bd44d20fc38
Jinadu Bunmi <jinadu_bunmi@yahoo.com>
2022-09-16 02:31:22 +0000

    updated

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_remaining_days - prints number of days left in a year
+ * print_remaining_days - prints the number of days left in a year
  * @month: month of year
- * @day: day of the year
+ * @day: day of month
  * @year: year
  */
 
@@ -22,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
