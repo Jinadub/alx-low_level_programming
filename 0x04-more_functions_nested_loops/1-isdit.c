5a7c8838997d473052600808177a919382c790d9
Jinadu Bunmi <jinadu_bunmi@yahoo.com>
2022-09-16 03:36:40 +0000

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - Checks for a digit
+ * @c: Character that is checked
+ * Return: 1 for success, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
