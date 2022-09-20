86b86d238ba20992b4b9dcb7eadd389bdcaa94c7
Jinadu Bunmi <jinadu_bunmi@yahoo.com>
2022-09-20 03:43:35 +0000

    updated 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/*
- * reset_to_98 - takes a pointer to an int and updates value
+/**
+ * reset_to_98 - takes a pointer to an int and updates its value
  * @n: Parameter that is updated
  * Return: void
  */
---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check functions.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I am a girl";
+
+	_puts(str);
+	return (0);
+
+}
