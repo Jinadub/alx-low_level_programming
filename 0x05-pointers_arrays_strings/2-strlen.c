dc57437e5ae6ae5daa5f24030541b67839683afd
Jinadu Bunmi <jinadu_bunmi@yahoo.com>
2022-09-20 04:06:31 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: String to be returned
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
+
