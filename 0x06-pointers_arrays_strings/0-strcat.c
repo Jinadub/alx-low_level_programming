0a43d4d3060216e79c53c0bad37da4d62c440627
Jinadu Bunmi <jinadu_bunmi@yahoo.com>
2022-09-23 03:46:09 +0000

    Task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Destination string
+ * @src: Source string
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; src[b] != '\0'; b++)
+	{
+		*(dest + a) = src[b];
+		a++;
+	}
+	*(dest + a) = '\0';
+
+	return (dest);
+}
